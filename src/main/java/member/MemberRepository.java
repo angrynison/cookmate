71ae55fcbc29d11a4555009d9a3b9d9d91b1d63f
kalina01255@naver.com <kalina01255@naver.com>
2026-06-18 19:23:51 +0900

    잡다한거 수정

--- src/main/java/member/MemberRepository.java
+++ src/main/java/member/MemberRepository.java
@@ -1,5 +1,6 @@
 package member;
 
+import member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
--- src/main/java/member/MemberService.java
+++ src/main/java/member/MemberService.java
@@ -2,5 +2,11 @@ package member;
 
 public class MemberService {
 
+    // 회원가입 중복 체크
+    
+    // 회원 생성
+
+    // 회원 로그아웃
+    
 
 }
