190c83ecd0ae4febd1ce0115571f00f565cfec3b
angrynison <kalina01255@gmail.com>
2026-06-29 15:41:39 +0900

    enum type 속성추가

--- src/main/java/global/type/StorageType.java
+++ src/main/java/global/type/StorageType.java
@@ -1,6 +1,7 @@
 package global.type;
 
 public enum StorageType {
+    기본,
     상온,
     냉장,
     냉동
--- src/main/java/global/type/Unit.java
+++ src/main/java/global/type/Unit.java
@@ -3,5 +3,6 @@ package global.type;
 public enum Unit {
     EA,
     ML,
-    KG
+    KG,
+    G,
 }
