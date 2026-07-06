14c21acf5e57b3be9dbdfe3a4e8cdb2cc1a113bd
kalina01255@naver.com <kalina01255@naver.com>
2026-07-06 22:27:12 +0900

    컬럼 속성 변경

--- src/main/java/member/domain/Member.java
+++ src/main/java/member/domain/Member.java
@@ -4,6 +4,9 @@ import global.type.Cuisine;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name= "member")
 @Getter
@@ -16,23 +19,31 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
-    @Column(nullable = false, length = 100)
+    @Column(length = 100)
     String name;
-    @Column(nullable = false)
-    String email;
+    @Column(nullable = false, unique = true)
+    String loginId;
     @Column(nullable = false)
     String password;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
     private Sex sex;
 
+    // 음식 선호도 다중 값을 저장하기 위한 memebr - memebr_cuisine의 many to one 매핑 테이블 생성
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(
+            name = "member_cuisine",
+            joinColumns = @JoinColumn(name = "member_id")
+    )
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private Cuisine cuisine;
-    
+    @Column(name = "cuisine_type")
+    private Set<Cuisine> cuisines = new HashSet<>();
+
+
+
     public enum Sex {
         남,
         녀
     }
+
 }
