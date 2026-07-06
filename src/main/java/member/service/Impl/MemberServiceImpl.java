33143947fd9eeba629a6d986692a1dbe76caebed
kalina01255@naver.com <kalina01255@naver.com>
2026-07-06 22:44:57 +0900

    MemberService 인터페이스&구현 클래스 작성

---
+++ src/main/java/member/service/Impl/MemberServiceImpl.java
@@ -0,0 +1,11 @@
+package member.service.Impl;
+
+import member.service.MemberService;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class MemberServiceImpl implements MemberService {
+
+
+}
