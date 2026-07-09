047f314ace9fa706466784a7ce0c15476dcb617b
kalina01255@naver.com <kalina01255@naver.com>
2026-07-09 19:10:24 +0900

    일부 response dto 주석처리

--- src/main/java/pantry/dto/PantryResponseDto.java
+++ src/main/java/pantry/dto/PantryResponseDto.java
@@ -15,19 +15,25 @@ public class PantryResponseDto {
 
     private PantryResponseDto() {}
 
+    /*
     // 백엔드 데이터(dashboard) 응답 반환
-    public record DashboardResponse(
+    public record PantriesResponse(
             Long id,
             String name,
             LocalDate expiryDate,
             StorageType storageType,
             Integer quantity
     ) {}
+     */
 
     // 화면 상단의 재료 요약 데이터 응답 반환
     public record SummaryResponse(
             Long totalItems,
             Long oldItems,
             Long freshItems
+    ) {}
+
+    public record returnId(
+            Long id
     ) {}
 }
