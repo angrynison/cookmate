93c72f012d3cb43401a017cb4c61b3a78b6282f7
kalina01255@naver.com <kalina01255@naver.com>
2026-07-09 19:14:47 +0900

    기본 식재료 리스트 반환 추가

--- src/main/java/ingredient/service/Impl/IngredientServiceImpl.java
+++ src/main/java/ingredient/service/Impl/IngredientServiceImpl.java
@@ -5,19 +5,16 @@ import ingredient.domain.Ingredient;
 import ingredient.repository.IngredientRepository;
 import ingredient.dto.IngredientRequestDto;
 import ingredient.service.IngredientService;
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 public class IngredientServiceImpl implements IngredientService {
 
     IngredientRepository ingredientRepository;
 
-
-    public IngredientServiceImpl(IngredientRepository ingredientRepository) {
-        this.ingredientRepository = ingredientRepository;
-    }
-
-
+    // 재료 기본 정보 등록
     @Override
     public Long saveIngredient(IngredientRequestDto.CreateRequest request) {
         Ingredient ingredient = Ingredient.create(
@@ -32,6 +29,7 @@ public class IngredientServiceImpl implements IngredientService {
         return ingredient.getId();
     }
 
+    // 기본 재료 정보 수정
     @Override
     public Long updateIngredient(Long ingredientId, IngredientRequestDto.UpdateRequest request) {
         Ingredient ingredient = ingredientRepository.findById(ingredientId)
@@ -48,13 +46,20 @@ public class IngredientServiceImpl implements IngredientService {
         return ingredient.getId();
     }
 
+    // 기본 재료 정보 삭제
     @Override
     public void deleteIngredient(Long pantryId) {
         ingredientRepository.deleteById(pantryId);
     }
 
+    // 기본 재료 정보 리스트 반환
     @Override
-    public List<Ingredient> getIngredientList(IngredientCategory category) {
+    public List<Ingredient> getIngredientList() {
+        return ingredientRepository.findAll();
+    }
+
+    // 기본 재료 정보 카테고리별 (리스트) 반환
+    public List<Ingredient> getIngredientListByCategory(IngredientCategory category) {
         return ingredientRepository.findByCategory(category);
     }
 }
--- src/main/java/ingredient/service/IngredientService.java
+++ src/main/java/ingredient/service/IngredientService.java
@@ -10,5 +10,6 @@ public interface IngredientService {
     Long saveIngredient(IngredientRequestDto.CreateRequest request);
     Long updateIngredient(Long ingredientId,IngredientRequestDto.UpdateRequest request);
     void deleteIngredient(Long pantryId);
-    List<Ingredient> getIngredientList(IngredientCategory category);
+    List<Ingredient> getIngredientList();
+    List<Ingredient> getIngredientListByCategory(IngredientCategory category);
 }
