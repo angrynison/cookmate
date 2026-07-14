302f2f69412864183023a9ab9cd93bb5bc37d922
kalina01255@naver.com <kalina01255@naver.com>
2026-07-14 18:55:01 +0900

    메소드명 수정

--- src/main/java/com/cookmate/member/MemberController.java
+++ src/main/java/com/cookmate/member/MemberController.java
@@ -20,20 +20,20 @@ public class MemberController {
 
     // 회원가입
     @PostMapping("/user/signup")
-    public ResponseEntity<Long> signup(@RequestBody MemberRequestDto.JoinRequest joinRequest) {
+    public ResponseEntity<Long> signupUser(@RequestBody MemberRequestDto.JoinRequest joinRequest) {
         Long memberId = memberService.join(joinRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(memberId);
     }
 
     // 로그인
     @PostMapping("/user/login")
-    public ResponseEntity<MemberResponseDto.JwtTokenResponse> login(@RequestBody MemberRequestDto.LoginRequest loginRequest) {
+    public ResponseEntity<MemberResponseDto.JwtTokenResponse> loginUser(@RequestBody MemberRequestDto.LoginRequest loginRequest) {
          return ResponseEntity.ok(memberService.login(loginRequest));
     }
 
     // 회원 프로필 생성
     @PostMapping("/user/profile")
-    public ResponseEntity<Long> profileRegister(@AuthenticationPrincipal UserDetails userDetails, @RequestBody MemberRequestDto.ProfileRequest profileRequest) {
+    public ResponseEntity<Long> createProfile(@AuthenticationPrincipal UserDetails userDetails, @RequestBody MemberRequestDto.ProfileRequest profileRequest) {
         Long id = Long.parseLong(userDetails.getUsername());
         Long memberId = memberService.createProfile(id, profileRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(memberId);
@@ -64,14 +64,14 @@ public class MemberController {
 
     // 관리자 등록
     @PostMapping ("/admin")
-    ResponseEntity<Long> adminSignup(@RequestBody MemberRequestDto.JoinRequest joinRequest) {
+    ResponseEntity<Long> signupAdmin(@RequestBody MemberRequestDto.JoinRequest joinRequest) {
         Long adminId = memberService.join(joinRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(adminId);
     }
 
     // 관리자 로그인
     @PostMapping("/admin/login")
-    ResponseEntity<MemberResponseDto.JwtTokenResponse> adminLogin(@RequestBody MemberRequestDto.LoginRequest loginRequest) {
+    ResponseEntity<MemberResponseDto.JwtTokenResponse> loginAdmin(@RequestBody MemberRequestDto.LoginRequest loginRequest) {
         MemberResponseDto.JwtTokenResponse jwtTokenResponse = memberService.login(loginRequest);
         return jwtTokenResponse != null ? ResponseEntity.ok(jwtTokenResponse) : ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
