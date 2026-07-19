15c679af4ff441471761dec2eeadc4c2b9595a6a
kalina01255@naver.com <kalina01255@naver.com>
2026-07-20 03:39:33 +0900

    응답 Response Dto 형식 추가

--- src/main/java/com/cookmate/ingredient/dto/IngredientResponseDto.java
+++ src/main/java/com/cookmate/ingredient/dto/IngredientResponseDto.java
@@ -1,10 +1,30 @@
 package com.cookmate.ingredient.dto;
 
+import com.cookmate.global.type.IngredientCategory;
+import com.cookmate.ingredient.domain.Ingredient;
+
 public class IngredientResponseDto {
 
+    public record IngredientResponse(
+            Long id,
+            String name,
+            Integer defaultExpiry,
+            Integer frozenExpiry,
+            Integer ambientExpiry,
+            Integer refrigeratedExpiry,
+            IngredientCategory ingredientCategory
+    ){
+        public static IngredientResponse from(Ingredient ingredient) {
+            return new IngredientResponse(
+                    ingredient.getId(),
+                    ingredient.getName(),
+                    ingredient.getDefaultExpiry(),
+                    ingredient.getFrozenExpiry(),
+                    ingredient.getAmbientExpiry(),
+                    ingredient.getRefrigeratedExpiry(),
+                    ingredient.getIngredientCategory()
+            );
+        }
 
-
-
-
-
+    };
 }
