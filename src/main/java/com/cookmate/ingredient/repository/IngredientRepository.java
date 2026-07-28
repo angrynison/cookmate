3733c074bd9fbac0d202263d4a7ce32f8a906b64
angry <kalina01255@naver.com>
2026-07-29 04:04:50 +0900

    repository 어노테이션 누락된것 추가

--- src/main/java/com/cookmate/ingredient/repository/IngredientRepository.java
+++ src/main/java/com/cookmate/ingredient/repository/IngredientRepository.java
@@ -5,9 +5,11 @@ import com.cookmate.ingredient.domain.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
 
     Ingredient findByName(String name);
